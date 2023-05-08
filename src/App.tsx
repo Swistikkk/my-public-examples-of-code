71d1a26c39f55cfada1dbf38fcb37cf8918359a2
Victor Oberemko <oberemkovictor@gmail.com>
2023-05-08 21:12:18 +0300

    Add routing and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Counter } from "./components/Counter";
+import { Link, Route, Routes } from "react-router-dom";
 import './index.scss';
+import { Suspense } from "react";
+import { AboutPageAsync } from "./pages/AboutPage/About.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            weaawdawdawdawdawd
-            <Counter />
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/about' element={<AboutPageAsync />} />
+                    <Route path="/" element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 };
